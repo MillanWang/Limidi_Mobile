9701d5af54ea5723d91dd97c73e14a53a16a171c
Millan <real.millan.wang@gmail.com>
2022-08-14 22:27:37 -0400

    Initial setup commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,18 +1,35 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  // Button
+} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
+import { Button } from "@rneui/themed";
+
+
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [sampleText, setSampleText] = useState("hey");
+
+  const x = () => {
+    console.log("BRUH2");
+    setSampleText(sampleText + "i")
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Button title="Nice Button" onPress={x} />
+      <Text style={styles.title}>{sampleText}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
