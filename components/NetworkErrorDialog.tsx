2ba42bad0cf8e95e4479901f5f056a4a91d7b588
Millan Wang <real.millan.wang@gmail.com>
2023-09-07 23:26:43 -0400

    barcode scanner

--- components/NetworkErrorDialog.tsx
+++ components/NetworkErrorDialog.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 import { Button, Dialog, Text } from "@rneui/themed";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { setMostRecentNetworkFixTime } from "../redux/slices/HttpCommunicationsSlice";
 import { useDesktopCommunication } from "../hooks/useDesktopCommunication";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Camera, CameraType } from "expo-camera";
 
 const MIN_TIME_BETWEEN_FIX = 5000;
 
@@ -22,13 +24,55 @@ export default function NetworkErrorDialog() {
             </View>
 
             <View style={styles.dialogContentContainer}>
-                <Text>YOOO</Text>
+                <ConnectionCodeScanner />
             </View>
 
             <View style={styles.saveButtonContainer}>
                 <Button onPress={fix}>Temporary fix</Button>
             </View>
         </Dialog>
+    );
+}
+
+export function ConnectionCodeScanner() {
+    const [hasPermission, setHasPermission] = useState(false);
+    const [scanData, setScanData] = useState<string | undefined>(undefined);
+
+    console.log(scanData);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, []);
+    if (!hasPermission) {
+        return (
+            <View>
+                <Text>No permissions</Text>
+            </View>
+        );
+    }
+    const handleBarCodeScanned = (result: { type: string; data: string }) => {
+        setScanData(result.data);
+        console.log(result.type);
+        console.log(result.data);
+    };
+
+    // BRUH HOW DOES THIS STILL SCAN WHEN INVISIBLE???
+
+    return (
+        <View>
+            <Text>hey</Text>
+            <Camera type={CameraType.back}>
+                <View
+                // style={styles.buttonContainer}
+                >
+                    <BarCodeScanner type="back" onBarCodeScanned={scanData ? undefined : handleBarCodeScanned} />
+                </View>
+            </Camera>
+            {scanData && <Button onPress={() => setScanData(undefined)}>scanAgain</Button>}
+        </View>
     );
 }
 
--- screens/GridScreen.tsx
+++ screens/GridScreen.tsx
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, } from 'react-native';
-import { GridElementGrid } from '../components/GridElementGrid';
-import { GridScreenToolbar } from '../components/GridScreenToolbar';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { GridElementGrid } from "../components/GridElementGrid";
+import { GridScreenToolbar } from "../components/GridScreenToolbar";
 
 export default function GridScreen() {
-
     const [isPlayMode, setIsPlayMode] = useState(true);
     const [showGridEditDialog, setShowGridEditDialog] = useState(false);
 
     return (
         <View style={styles.container}>
             <GridScreenToolbar
-                isPlayMode={isPlayMode} setIsPlayMode={setIsPlayMode}
-                isVisible={showGridEditDialog} setIsVisible={setShowGridEditDialog}
+                isPlayMode={isPlayMode}
+                setIsPlayMode={setIsPlayMode}
+                isVisible={showGridEditDialog}
+                setIsVisible={setShowGridEditDialog}
             />
             <GridElementGrid isPlayMode={isPlayMode} />
         </View>
-
     );
 }
-
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 30,
-        backgroundColor: '#000000'
+        backgroundColor: "#000000",
     },
 });
