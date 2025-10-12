6322af4f0d094c36d838724949c12b77698a1321
Millan Wang <real.millan.wang@gmail.com>
2025-10-11 23:02:15 -0400

    Multislider experimentation

--- components/GridElement/GridElementEditDialog/GridElementEditDialogTabs/velocitySettings/VelocityAdjustSlider.tsx
+++ components/GridElement/GridElementEditDialog/GridElementEditDialogTabs/velocitySettings/VelocityAdjustSlider.tsx
@@ -1,3 +1,4 @@
+import MultiSlider from "@ptomasroos/react-native-multi-slider";
 import { Slider, Text } from "@rneui/themed";
 import React from "react";
 import { View } from "react-native";
@@ -10,28 +11,40 @@ export const VelocityAdjustSlider = (props: {
 }) => {
   const { velocity, setVelocity, backgroundColor, textColor } = props;
   return (
-    <Slider
-      maximumValue={127}
-      minimumValue={0}
-      step={1}
-      value={velocity}
-      onValueChange={setVelocity}
-      maximumTrackTintColor={backgroundColor}
-      minimumTrackTintColor={textColor}
-      thumbStyle={{ backgroundColor, borderWidth: 1, borderColor: textColor }}
-      thumbProps={{
-        children: (
-          <View
-            style={{
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100%",
-            }}
-          >
-            <Text style={{ color: textColor }}>{velocity}</Text>
-          </View>
-        ),
-      }}
-    />
+    <>
+      <Slider
+        maximumValue={127}
+        minimumValue={0}
+        step={1}
+        value={velocity}
+        onValueChange={setVelocity}
+        maximumTrackTintColor={backgroundColor}
+        minimumTrackTintColor={textColor}
+        thumbStyle={{ backgroundColor, borderWidth: 1, borderColor: textColor }}
+        thumbProps={{
+          children: (
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <Text style={{ color: textColor }}>{velocity}</Text>
+            </View>
+          ),
+        }}
+      />
+      <MultiSlider
+        isMarkersSeparated
+        values={[velocity, 127]}
+        onValuesChange={(e) => {
+          console.log("e", e);
+        }}
+        min={0}
+        max={127}
+        step={1}
+      />
+    </>
   );
 };
